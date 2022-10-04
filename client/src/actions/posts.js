80112154eab45342fb3d501dcd866404aa066b3d
Cat <catholm1@outlook.com>
2022-10-04 18:57:08 +0200

    first

---
+++ client/src/actions/posts.js
@@ -0,0 +1,57 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import * as api from '../api/index.js';
+
+
+//action Creators that are functions that return actions
+export const getPosts = () => async (dispatch) => {
+    try {
+      const { data } = await api.fetchPosts();
+
+      dispatch({ type: FETCH_ALL, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+      const { data } = await api.createPost(post);
+
+      dispatch({ type: CREATE, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+      const { data } = await api.updatePost(id, post);
+
+      dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+
+  try {
+
+      dispatch({ type: DELETE, payload:  id  });
+
+  } catch (error) {
+    
+    console.log(error);
+
+  }
+}
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
